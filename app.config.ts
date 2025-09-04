2c094c38fb4985eed625b87061c75c79e6f2047c
awakebrasil <56883395+awakebrasil@users.noreply.github.com>
2025-09-04 07:53:15 -0300

    Add files via upload

---
+++ app.config.ts
@@ -0,0 +1,102 @@
+import { ExpoConfig, ConfigContext } from 'expo/config';
+
+export default ({ config }: ConfigContext): ExpoConfig => {
+  return {
+    name: 'Chatwoot',
+    slug: process.env.EXPO_PUBLIC_APP_SLUG || 'chatwoot-mobile',
+    version: '4.1.2',
+    orientation: 'portrait',
+    icon: './assets/icon.png',
+    userInterfaceStyle: 'light',
+    newArchEnabled: false,
+    splash: {
+      image: './assets/splash.png',
+      resizeMode: 'contain',
+      backgroundColor: '#ffffff',
+      enableFullScreenImage_legacy: true,
+    },
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: 'com.chatwoot.app',
+      infoPlist: {
+        NSCameraUsageDescription:
+          'This app requires access to the camera to upload images and videos.',
+        NSPhotoLibraryUsageDescription:
+          'This app requires access to the photo library to upload images.',
+        NSMicrophoneUsageDescription: 'This app requires access to the microphone to record audio.',
+        NSAppleMusicUsageDescription:
+          'This app does not use Apple Music, but a system API may require this permission.',
+        UIBackgroundModes: ['fetch', 'remote-notification'],
+        ITSAppUsesNonExemptEncryption: false,
+      },
+      // Please use the relative path to the google-services.json file
+      googleServicesFile: process.env.EXPO_PUBLIC_IOS_GOOGLE_SERVICES_FILE,
+      entitlements: { 'aps-environment': 'production' },
+      associatedDomains: ['applinks:app.chatwoot.com'],
+    },
+    android: {
+      adaptiveIcon: { foregroundImage: './assets/adaptive-icon.png', backgroundColor: '#ffffff' },
+      package: 'com.chatwoot.app',
+      permissions: [
+        'android.permission.CAMERA',
+        'android.permission.READ_EXTERNAL_STORAGE',
+        'android.permission.WRITE_EXTERNAL_STORAGE',
+        'android.permission.RECORD_AUDIO',
+        'android.permission.READ_MEDIA_IMAGES',
+      ],
+      // Please use the relative path to the google-services.json file
+      googleServicesFile: process.env.EXPO_PUBLIC_ANDROID_GOOGLE_SERVICES_FILE,
+      intentFilters: [
+        {
+          action: 'VIEW',
+          autoVerify: true,
+          data: [
+            {
+              scheme: 'https',
+              host: 'app.chatwoot.com',
+              pathPrefix: '/app/accounts/',
+              pathPattern: '/*/conversations/*',
+            },
+          ],
+          category: ['BROWSABLE', 'DEFAULT'],
+        },
+      ],
+    },
+    extra: {
+      eas: {
+        projectId: process.env.EXPO_PUBLIC_PROJECT_ID,
+        storybookEnabled: process.env.EXPO_STORYBOOK_ENABLED,
+      },
+    },
+    owner: 'chatwoot',
+    plugins: [
+      'expo-font',
+      ['react-native-permissions', { iosPermissions: ['Camera', 'PhotoLibrary', 'MediaLibrary'] }],
+      [
+        '@sentry/react-native/expo',
+        {
+          url: 'https://sentry.io/',
+          project: process.env.EXPO_PUBLIC_SENTRY_PROJECT_NAME,
+          organization: process.env.EXPO_PUBLIC_SENTRY_ORG_NAME,
+        },
+      ],
+      '@react-native-firebase/app',
+      '@react-native-firebase/messaging',
+      [
+        'expo-build-properties',
+        {
+          // https://github.com/invertase/notifee/issues/808#issuecomment-2175934609
+          android: {
+            minSdkVersion: 24,
+            compileSdkVersion: 35,
+            targetSdkVersion: 34,
+            enableProguardInReleaseBuilds: true,
+          },
+          ios: { useFrameworks: 'static' },
+        },
+      ],
+      './with-ffmpeg-pod.js',
+    ],
+    androidNavigationBar: { backgroundColor: '#ffffff' },
+  };
+};
